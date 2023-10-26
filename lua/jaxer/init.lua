43713875635dcb09f004ebc109a675dc939cbacf
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-10-26 16:45:46 -0700

    Lualine

--- lua/jaxer/init.lua
+++ lua/jaxer/init.lua
@@ -1,3 +1,4 @@
 require("jaxer.remap")
-
+require("jaxer.packer")
 vim.cmd('colorscheme kanagawa-dragon')
+
--- lua/jaxer/remap.lua
+++ lua/jaxer/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 vim.api.nvim_set_keymap('n', '<leader>w', ':wincmd w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
 
