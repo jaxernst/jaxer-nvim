be639b5c2f522a617df1622a0e2709765c788a78
jackson <jaxernst@gmail.com>
2023-10-26 21:17:17 -0700

    V map

--- lua/jaxer/remap.lua
+++ lua/jaxer/remap.lua
@@ -3,6 +3,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap('n', '<leader>w', ':wincmd w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
 
+vim.api.nvim_set_keymap('n', 'vj', '4j', { noremap = true })
+vim.api.nvim_set_keymap('n', 'vk', '4k', { noremap = true })
+vim.api.nvim_set_keymap('n', 'vh', '8h', { noremap = true })
+vim.api.nvim_set_keymap('n', 'vl', '8l', { noremap = true })
+
 
 
 
