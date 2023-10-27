0355c91cdecc941515832c04391c74d8251f9b38
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-10-27 15:40:56 -0700

    Copy paste

--- lua/jaxer/remap.lua
+++ lua/jaxer/remap.lua
@@ -4,9 +4,12 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap('n', '<leader>w', ':wincmd w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
 
+vim.api.nvim_set_keymap('n', ' sa', 'ggVG', { noremap = true })
 
+-- Normal mode mapping to yank to system clipboard
+vim.api.nvim_set_keymap('n', '<Leader>y', '"+y', { noremap = true, silent = true })
 
-
-
+-- Visual mode mapping to yank selected text to system clipboard
+vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', { noremap = true, silent = true })
 
 
