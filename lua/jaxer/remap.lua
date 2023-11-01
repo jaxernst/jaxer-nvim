67a6283b748e07d80e879a5b802e373d0fad9892
jackson <jaxernst@gmail.com>
2023-10-31 23:09:16 -0700

    Add window nav remaps

--- lua/jaxer/remap.lua
+++ lua/jaxer/remap.lua
@@ -13,3 +13,11 @@ vim.api.nvim_set_keymap('n', '<Leader>y', '"+y', { noremap = true, silent = true
 vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', { noremap = true, silent = true })
 
 
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+
+-- Navigate splits using Ctrl-h/j/k/l
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
