d0b93325b07d18fa2756397270094fea126d2f4a
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-11-01 21:00:57 -0700

    Remove harpoon quick navs

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,7 +7,3 @@ vim.keymap.set("n", "<leader>ph", ui.toggle_quick_menu)
 vim.keymap.set("n", "<leader>pj", function() ui.nav_next() end)
 vim.keymap.set("n", "<leader>pk", function() ui.nav_prev() end)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
