ade4fb76c82060268f21e4eb707df8bab7c56097
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-11-01 20:59:42 -0700

    Tab sizing

--- lua/jaxer/init.lua
+++ lua/jaxer/init.lua
@@ -2,15 +2,24 @@ require("jaxer.remap")
 require("jaxer.packer")
 
 vim.cmd('set relativenumber')
-vim.cmd('colorscheme kanagawa-dragon')
+vim.cmd('colorscheme kanagawa-wave')
 
 vim.wo.number = true
 
+-- Change rel -> absolute line numbers when switching modes --
 vim.api.nvim_exec([[
-  augroup numbertoggle
+   augroup numbertoggle
     autocmd!
     autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
     autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
   augroup END
 ]], false)
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
