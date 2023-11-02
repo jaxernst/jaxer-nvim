078852cbeb112c530141ff0d8658e412f05f4328
jackson <jaxernst@gmail.com>
2023-11-01 23:41:10 -0700

    Add formatter

--- lua/jaxer/packer.lua
+++ lua/jaxer/packer.lua
@@ -1,64 +1,67 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
 
-	use 'sharkdp/fd'
+  use("sharkdp/fd")
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    -- or                            , branch = '0.1.x',
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-	use('nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
+  use("ThePrimeagen/vim-be-good")
 
-	use("ThePrimeagen/vim-be-good")
-	
-	use("ThePrimeagen/harpoon")
+  use("ThePrimeagen/harpoon")
 
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-	}
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "nvim-tree/nvim-web-devicons", opt = true },
+  })
 
-	use "rktjmp/lush.nvim"
+  use("rktjmp/lush.nvim")
 
-	use "rebelot/kanagawa.nvim"
+  use("rebelot/kanagawa.nvim")
 
-	use "JoranHonig/tree-sitter-solidity"
+  use("JoranHonig/tree-sitter-solidity")
 
-	use "Himujjal/tree-sitter-svelte"
+  use("Himujjal/tree-sitter-svelte")
 
-	use 'mbbill/undotree'
+  use("mbbill/undotree")
 
-	use 'tpope/vim-fugitive'
+  use("tpope/vim-fugitive")
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+  -- LSP --
+  use({
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" }, -- Required
+      { "williamboman/mason.nvim" }, -- Optional
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	}
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" }, -- Required
+      { "hrsh7th/cmp-nvim-lsp" }, -- Required
+      { "L3MON4D3/LuaSnip" }, -- Required
+    },
+  })
 
-	use "nvim-tree/nvim-web-devicons"
+  use({ "mhartington/formatter.nvim" })
 
-	use {
-		'stevearc/oil.nvim',
-		config = function() require('oil').setup() end
-	}
+  use({
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup()
+    end,
+  })
 end)
